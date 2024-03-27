6cc45b6a4b25980815effe63818f5ee857162443
Suvansh Sanjeev <suvansh.sanjeev@gmail.com>
2024-03-27 08:43:04 -0400

    fixing InputExample bug, adding scoring flexibility to finetune launcher

--- adapt_embed/datasets/inputexample.py
+++ adapt_embed/datasets/inputexample.py
@@ -7,23 +7,31 @@ class InputExampleDataset(Dataset):
     """
     Wraps a dataset to create a dataset of InputExamples for compatibility with SBERT cross-encoders
     """
-    def __init__(self, dataset):
+    def __init__(self, dataset, score_triplet=False):
         self.dataset = dataset
         if isinstance(self.dataset, TripletDataset):
-            self.type = "triplet"
+            if score_triplet:
+                self.type = "triplet_score"
+            else:
+                self.type = "triplet"
         elif isinstance(self.dataset, PairwiseScoreDataset):
             self.type = "pairwise"
         else:
             raise ValueError("Unsupported dataset type")
 
     def __len__(self):
-        return len(self.dataset) if self.type == "pairwise" else len(self.dataset) * 2
+        return len(self.dataset) * 2 if self.type == "triplet_score" else len(self.dataset)
     
     def __getitem__(self, idx):
         if self.type == "pairwise":
             query, doc, score = self.dataset[idx]
             return InputExample(texts=[query, doc], label=float(score))
-        else:
-            query, good_doc, bad_doc = self.dataset.data[idx//2]
+        elif self.type == "triplet_score":
+            query, good_doc, bad_doc = self.dataset[idx//2]
             score = 1 if idx % 2 == 0 else 0
             return InputExample(texts=[query, good_doc if idx % 2 == 0 else bad_doc], label=float(score))
+        elif self.type == "triplet":
+            query, good_doc, bad_doc = self.dataset[idx]
+            return InputExample(texts=[query, good_doc, bad_doc])
+        else:
+            raise ValueError(f"Unsupported dataset type: {self.type}")
--- adapt_embed/models/finetune/run_finetune_launcher.py
+++ adapt_embed/models/finetune/run_finetune_launcher.py
@@ -26,21 +26,37 @@ def run_experiment(variant):
     num_epochs = variant['num_epochs']
     batch_size = variant['batch_size']
     lr = variant['lr']
-    loss_type = variant['loss_type']
     results_files = variant.get('results_files', [])
     results_every = variant.get('results_every', 0)
+    data_negative_sampling = variant.get('data_negative_sampling', True)
+    data_synthetic_gen = variant.get('data_synthetic_gen', False)
+    data_augmentation_threshold = variant.get('data_augmentation_threshold', 10)
+    score_triplet = variant.get('score_triplet', False)
+    loss_type = variant.get('loss_type', None)
+    triplet_margin = variant.get('triplet_margin', None)
 
     model = SentenceTransformer(model_name, device=device)
     finetuned_model = SentenceTransformer(model_name, device=device)
     torch_loss = {'mse': torch.nn.MSELoss(), 'bce': torch.nn.BCELoss()}.get(loss_type, None)
-    if torch_loss is None:
-        raise ValueError(f"Unknown loss type: {loss_type}. Must be one of 'mse' or 'bce'.")
+
+    if not score_triplet and triplet_margin is None:
+        raise ValueError("Must provide triplet_margin if not scoring triplet loss.")
+    if score_triplet and torch_loss is None:
+        raise ValueError("Must provide known loss_type if scoring triplet loss. Options: 'mse', 'bce'.")
 
     dataset = None
     def get_dataset():
         nonlocal dataset
         if dataset is None:
-            dataset = InputExampleDataset(TripletDataset(MTEB(tasks=[task]).tasks[0], negative_sampling=True, split=split, relevance_threshold=0.5))
+            dataset = InputExampleDataset(
+                        TripletDataset(MTEB(tasks=[task]).tasks[0],
+                                     split=split, relevance_threshold=0.5,
+                                     negative_sampling=data_negative_sampling,
+                                     synthetic_data=data_synthetic_gen,
+                                     data_augmentation_threshold=data_augmentation_threshold,
+                                     score_triplet=score_triplet
+                            )
+                        )
         return dataset
     
     def get_results(model, task):
@@ -53,9 +69,14 @@ def run_experiment(variant):
         os.makedirs(save_dir, exist_ok=True)
         print("Finetuning the model...")
         train_data = DataLoader(get_dataset(), shuffle=True, batch_size=batch_size)
-        valid_data = zip(*[(*dataset[i].texts, dataset[i].label) for i in range(0, 2501, 5)])  # 500 pairs, offset is odd to get pos and neg labels since they alternate
-        evaluator = evaluation.EmbeddingSimilarityEvaluator(*valid_data, name=task)
-        loss_fn = losses.CosineSimilarityLoss(model, loss_fct=torch_loss)
+        if score_triplet:
+            valid_data = zip(*[(*dataset[i].texts, dataset[i].label) for i in range(0, 2501, 5)])  # 500 pairs, offset is odd to get pos and neg labels since they alternate
+            evaluator = evaluation.EmbeddingSimilarityEvaluator(*valid_data, name=task, batch_size=batch_size)
+            loss_fn = losses.CosineSimilarityLoss(model, loss_fct=torch_loss)
+        else:
+            valid_data = zip(*[dataset[i].texts for i in range(0, 2501, 5)])
+            evaluator = evaluation.TripletEvaluator(*valid_data, name=task, batch_size=batch_size)
+            loss_fn = losses.TripletLoss(model, triplet_margin=triplet_margin)
         evaluation_steps = len(train_data) * max(0, results_every)
         evaluation_dicts = []
         callback = lambda score, epoch, steps: evaluation_dicts.append({'epoch': epoch, 'loss': score, 'steps': steps})
@@ -67,10 +88,10 @@ def run_experiment(variant):
             optimizer_params={'lr': lr}
         )
         results = get_results(model, task)
-        logger.record_dict(**evaluation_dicts[-1], **results[task][split])
+        logger.record_dict({**evaluation_dicts[-1], **results[task][split]})
         logger.dump_tabular()
         for eval_dict in evaluation_dicts[:-1]:
-            logger.record_dict(**eval_dict)
+            logger.record_dict(eval_dict)
             logger.dump_tabular()
         return results
     
@@ -86,27 +107,47 @@ def run_experiment(variant):
                     exp_name, variant)
 
 if __name__ == "__main__":
+    """ hyperparameters """
+    # triplet loss
     variants = dict(
+        exp_prefix=['finetune'],
         model_name=["all-MiniLM-L6-v2"],
         task=['CQADupstackEnglishRetrieval'],
         split=['test'],
-        num_epochs=[15],
-        batch_size=[256],
-        lr=[0, 1e-5, 3e-5, 1e-4],
-        loss_type=['mse', 'bce']
+        data_augmentation_threshold=[5],
+        num_epochs=[12],
+        batch_size=[128],
+        lr=[3e-3, 1e-3, 3e-4, 1e-4, 3e-5],
+        score_triplet=[False],
+        triplet_margin=[1/3, 1, 3]
     )
+    # converting triplets to classification and using loss on cosine similarity
+    # variants = dict(
+    #     exp_prefix=['finetune-triplet-score'],
+    #     model_name=["all-MiniLM-L6-v2"],
+    #     task=['CQADupstackEnglishRetrieval'],
+    #     split=['test'],
+    #     data_augmentation_threshold=[5],
+    #     num_epochs=[15],
+    #     batch_size=[128],
+    #     lr=[3e-5, 1e-4, 3e-4, 1e-3, 3e-3],
+    #     score_triplet=[True],
+    #     loss_type=['mse', 'bce']
+    # )
     results_files = []
+    results_every = 3
 
     search_space = {}
     sweeper = DeterministicHyperparameterSweeper(variants)
 
+    """ run experiments """
     for exp_id, variant in enumerate(sweeper.iterate_hyperparameters()):
         variant['results_files'] = results_files
-        variant['results_every'] = 3
+        variant['results_every'] = results_every
         launcher_util.run_experiment(
             run_experiment,
             variant=variant,
-            exp_prefix='finetune',
+            exp_prefix=variant['exp_prefix'],
             mode='local',
             snapshot_mode='last',
             base_log_dir=os.path.join(proj_dir, 'results', 'logs')

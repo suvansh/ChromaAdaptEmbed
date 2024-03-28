6c2ed611cd0b21a55d96fba08e5dd76d0316c9fd
Suvansh Sanjeev <suvansh.sanjeev@gmail.com>
2024-03-28 16:40:20 -0400

    added save/load functionality and synthetic data integration. really need a refactor to strip out the redundant code in the two datasets though

--- adapt_embed/datasets/pairwise.py
+++ adapt_embed/datasets/pairwise.py
@@ -1,11 +1,15 @@
 # TODO (suvansh): once things stabilize, figure out the right abstractions. Currently a lot of duplicate code with triplet.py
+import os
+import json
+from datetime import datetime
+import random
 import torch
 from torch.utils.data import Dataset
 from mteb.abstasks import AbsTaskRetrieval
 from mteb.evaluation.evaluators import DenseRetrievalExactSearch, DRESModel
 from sentence_transformers import SentenceTransformer
 
-from adapt_embed.utils import stringify_corpus_item, get_device
+from adapt_embed.utils import gen_synthetic_data, stringify_corpus_item, get_device, get_proj_dir
 
 
 class PairwiseScoreDataset(Dataset):
@@ -18,7 +22,9 @@ class PairwiseScoreDataset(Dataset):
                  relevance_threshold=None,
                  negative_sampling=True,
                  synthetic_data=False,
+                 data_augmentation_threshold=5,
                  split=None,
+                 load_dir=None,
                  **load_data_kwargs):
         """
         Creates a dataset of query-document pairs and their relevance scores
@@ -31,57 +37,83 @@ class PairwiseScoreDataset(Dataset):
         :param relevance_threshold: float representing the absolute threshold for relevance. if set, overrides proportional_relevance_threshold
         :param negative_sampling: bool representing whether to sample negative examples
         :param synthetic_data: bool representing whether to use synthetic data to generate positives. currently unused
+        :param data_augmentation_threshold: int representing the number of relevant docs below which to augment data. only used if negative_sampling or synthetic_data is True
         :param split: str representing the split to use. defaults to the last eval split in the retrieval task
+        :param load_dir: str representing the directory to load the dataset from
         :param load_data_kwargs: additional keyword arguments to pass to the retrieval task's load_data method
         """
-        self.retrieval_task = retrieval_task
-        self.retrieval_task.load_data(**load_data_kwargs)
-        
-        if thresholded and normalized:
-            raise ValueError("Cannot set both normalized and thresholded to True")
-        self.normalized = normalized
-        self.thresholded = thresholded
-        
-        self.proportional_relevance_threshold = proportional_relevance_threshold
-        self.relevance_threshold = relevance_threshold
-        self.negative_sampling = negative_sampling
-        self.synthetic_data = synthetic_data
-        
-        if self.relevance_threshold is None:
-            self.set_threshold()
+        if retrieval_task is None:
+            if load_dir is None:
+                raise ValueError("Either retrieval_task or load_dir must be provided.")
+            else:
+                attributes_path = os.path.join(load_dir, 'attributes.json')
+                data_path = os.path.join(load_dir, 'data.json')
+                positive_doc_ids_path = os.path.join(load_dir, 'positive_doc_ids.json')
+                negative_doc_ids_path = os.path.join(load_dir, 'negative_doc_ids.json')
+                
+                if not os.path.exists(attributes_path) or not os.path.exists(data_path):
+                    raise FileNotFoundError("The specified directory does not contain the required files.")
+                
+                with open(attributes_path, 'r') as f:
+                    attributes = json.load(f)
+                    for key, value in attributes.items():
+                        setattr(self, key, value)
+                
+                with open(data_path, 'r') as f:
+                    self.data = json.load(f)
+                
+                with open(positive_doc_ids_path, 'r') as f:
+                    self.positive_doc_ids = json.load(f)
+                with open(negative_doc_ids_path, 'r') as f:
+                    self.negative_doc_ids = json.load(f)
+        else:
+            self.retrieval_task = retrieval_task
+            self.task_description = retrieval_task.description
+            self.retrieval_task.load_data(**load_data_kwargs)
+            
+            if thresholded and normalized:
+                raise ValueError("Cannot set both normalized and thresholded to True")
+            self.normalized = normalized
+            self.thresholded = thresholded
+            
+            self.proportional_relevance_threshold = proportional_relevance_threshold
+            if relevance_threshold is None:
+                self.set_threshold()
+            else:
+                self.relevance_threshold = relevance_threshold
+                self.min_relevance_score = 0
+                self.max_relevance_score = 1
+            self.negative_sampling = negative_sampling
+            self.synthetic_data = synthetic_data
+            self.data_augmentation_threshold = data_augmentation_threshold
 
-        self.split = split or self.retrieval_task.description['eval_splits'][-1]
-        query_ids = self.retrieval_task.queries[self.split].keys()
-        self.data = []
+            self.split = split or self.retrieval_task.description['eval_splits'][-1]
+            query_ids = self.retrieval_task.queries[self.split].keys()
+            self.data = []
 
-        if self.negative_sampling or self.synthetic_data:
-            if dres is None:
-                default_embed = SentenceTransformer("all-MiniLM-L6-v2", device=get_device())
-                dres = DenseRetrievalExactSearch(DRESModel(default_embed))
-            if not dres.results:
-                dres.search(self.retrieval_task.corpus[self.split], self.retrieval_task.queries[self.split], top_k=100, score_function='cos_sim', return_sorted=True)
-        
-        # if self.negative_sampling, use dres.results to augment negative data. if self.synthetic_data then positive data
-        negatives = {}
-        positives = {}
-        for query_id in query_ids:
-            relevant_docs = self.retrieval_task.relevant_docs[self.split][query_id]
-            negatives[query_id] = {(doc_id, score) for doc_id, score in relevant_docs.items() if score < self.relevance_threshold}
-            positives[query_id] = {(doc_id, score) for doc_id, score in relevant_docs.items() if score >= self.relevance_threshold}
-            if self.negative_sampling and len(negatives[query_id]) < 5:
-                for doc_id, score in dres.results[query_id].items():
-                    if score < self.relevance_threshold:
-                        negatives[query_id].add((doc_id, score))
-                    if len(negatives[query_id]) >= 5:
-                        break
-            if self.synthetic_data and len(positives[query_id]) < 5:
-                pass
-            for (doc_id, score) in positives[query_id]:
-                self.data.append((query_id, doc_id, score))
-            for (doc_id, score) in negatives[query_id]:
-                self.data.append((query_id, doc_id, score))
-        self.negative_doc_ids = negatives
-        self.positive_doc_ids = positives
+            """ Create a list of scored pairs for each query. """
+            # if negative_sampling, augment negatives up to data_augmentation_threshold.
+            # if synthetic_data, augment positives up to data_augmentation_threshold.
+            negatives = {}
+            positives = {}
+            for query_id in query_ids:
+                relevant_docs = self.retrieval_task.relevant_docs[self.split][query_id]
+                negatives[query_id] = {(doc_id, score, False) for doc_id, score in relevant_docs.items() if score < self.relevance_threshold}  # False indicates doc_id
+                positives[query_id] = {(doc_id, score, False) for doc_id, score in relevant_docs.items() if score >= self.relevance_threshold}  # False indicates doc_id
+                if self.negative_sampling and (neg_docs_left := self.data_augmentation_threshold - len(negatives[query_id])) > 0:
+                    random_docs = random.sample(list(self.retrieval_task.corpus[self.split].keys()), neg_docs_left)
+                    for doc_id in random_docs:
+                        negatives[query_id].add((doc_id, self.min_relevance_score, False))  # False indicates doc_id
+                if self.synthetic_data and (pos_docs_left := self.data_augmentation_threshold - len(positives[query_id])) > 0:
+                    synthetic_docs = gen_synthetic_data(self.retrieval_task.queries[self.split][query_id], pos_docs_left)
+                    for doc in synthetic_docs:
+                        positives[query_id].add((doc, self.max_relevance_score, True))  # True indicates a document itself
+                for (doc_id_or_doc, score, is_doc) in positives[query_id]:
+                    self.data.append((query_id, doc_id_or_doc, score, is_doc))
+                for (doc_id_or_doc, score, is_doc) in negatives[query_id]:
+                    self.data.append((query_id, doc_id_or_doc, score, is_doc))
+            self.negative_doc_ids = negatives
+            self.positive_doc_ids = positives
     
     def set_threshold(self: float):
         """
@@ -107,13 +139,53 @@ class PairwiseScoreDataset(Dataset):
         return len(self.data)
     
     def __getitem__(self, idx):
-        query_id, doc_id, score = self.data[idx]
+        query_id, doc_id_or_doc, score, is_doc = self.data[idx]
+        doc_content = doc_id_or_doc if is_doc else stringify_corpus_item(self.retrieval_task.corpus[self.split][doc_id_or_doc])
+        
         if self.thresholded:
             score = 1 if score >= self.relevance_threshold else 0
         elif self.normalized:
             score = (score - self.min_relevance_score) / (self.max_relevance_score - self.min_relevance_score)
+        
         return (
             self.retrieval_task.queries[self.split][query_id],
-            stringify_corpus_item(self.retrieval_task.corpus[self.split][doc_id]),
+            doc_content,
             score
         )
+
+    def save(self, directory=None):
+        if directory is None:
+            directory = os.path.join(get_proj_dir(), 'datasets', 'cached', self.retrieval_task.description['task'], self.split, 'pairwise', datetime.now().strftime('%Y-%m-%d-%H-%M-%S'))
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        
+        # Save attributes
+        attributes = {
+            'proportional_relevance_threshold': self.proportional_relevance_threshold,
+            'relevance_threshold': self.relevance_threshold,
+            'normalized': self.normalized,
+            'thresholded': self.thresholded,
+            'min_relevance_score': self.min_relevance_score,
+            'max_relevance_score': self.max_relevance_score,
+            'negative_sampling': self.negative_sampling,
+            'synthetic_data': self.synthetic_data,
+            'data_augmentation_threshold': self.data_augmentation_threshold,
+            'split': self.split,
+            'task_description': self.task_description
+        }
+        with open(os.path.join(directory, 'attributes.json'), 'w') as f:
+            json.dump(attributes, f, indent=4)
+        
+        # Save dataset
+        with open(os.path.join(directory, 'data.json'), 'w') as f:
+            json.dump(self.data, f, indent=None)
+        
+        # Save positive and negative doc ids
+        with open(os.path.join(directory, 'positive_doc_ids.json'), 'w') as f:
+            json.dump(self.positive_doc_ids, f, indent=None)
+        with open(os.path.join(directory, 'negative_doc_ids.json'), 'w') as f:
+            json.dump(self.negative_doc_ids, f, indent=None)
+
+    @classmethod
+    def load(cls, directory):
+        return cls(load_dir=directory)
--- adapt_embed/datasets/triplet.py
+++ adapt_embed/datasets/triplet.py
@@ -1,28 +1,33 @@
 # TODO (suvansh): once things stabilize, figure out the right abstractions. Currently a lot of duplicate code with pairwise.py
+from typing import Optional
 import torch
+import os
+import json
 import random
+from datetime import datetime
 from torch.utils.data import Dataset
 from mteb.abstasks import AbsTaskRetrieval
 from mteb.evaluation.evaluators import DenseRetrievalExactSearch, DRESModel
 from sentence_transformers import SentenceTransformer
 
-from adapt_embed.utils import stringify_corpus_item, get_device, gen_synthetic_data
+from adapt_embed.utils import get_proj_dir, stringify_corpus_item, get_device, gen_synthetic_data
 
 
 class TripletDataset(Dataset):
     def __init__(self,
-                 retrieval_task: AbsTaskRetrieval,
-                 dres: DenseRetrievalExactSearch | None = None,
+                 retrieval_task: Optional[AbsTaskRetrieval] = None,
+                 dres: Optional[DenseRetrievalExactSearch] = None,
                  proportional_relevance_threshold=0.5,
                  relevance_threshold=None,
                  negative_sampling=True,
                  synthetic_data=False,
                  data_augmentation_threshold=5,
                  split=None,
+                 load_dir=None,
                  **load_data_kwargs):
         """
         Creates a dataset of triplets of (query, good_document, bad_document) for training a triplet loss model.
-        :param retrieval_task: AbsTaskRetrieval from MTEB
+        :param retrieval_task: AbsTaskRetrieval from MTEB.
         :param dres: DenseRetrievalExactSearch from MTEB used to compute query-document similarity
         :param proportional_relevance_threshold: float in [0, 1] representing the threshold for relevance
         :param proportional_relevance_threshold: float in [0, 1] representing the threshold for relevance.
@@ -30,55 +35,76 @@ class TripletDataset(Dataset):
         :param relevance_threshold: float representing the absolute threshold for relevance. if set, overrides proportional_relevance_threshold
         :param negative_sampling: bool representing whether to sample negative examples
         :param synthetic_data: bool representing whether to use synthetic data to generate positives. currently unused
-        :param data_augmentation_threshold: int representing the number of relevant docs below which to augment data
+        :param data_augmentation_threshold: int representing the number of relevant docs below which to augment data. only used if negative_sampling or synthetic_data is True
         :param split: str representing the split to use. defaults to the last eval split in the retrieval task
+        :param load_dir: str representing the directory to load the dataset from
         :param load_data_kwargs: additional keyword arguments to pass to the retrieval task's load_data method
         """
-        self.retrieval_task = retrieval_task
-        self.retrieval_task.load_data(**load_data_kwargs)
+        if retrieval_task is None:
+            if load_dir is None:
+                raise ValueError("Either retrieval_task or load_dir must be provided.")
+            else:
+                attributes_path = os.path.join(load_dir, 'attributes.json')
+                data_path = os.path.join(load_dir, 'data.json')
+                positive_doc_ids_path = os.path.join(load_dir, 'positive_doc_ids.json')
+                negative_doc_ids_path = os.path.join(load_dir, 'negative_doc_ids.json')
+                
+                if not os.path.exists(attributes_path) or not os.path.exists(data_path):
+                    raise FileNotFoundError("The specified directory does not contain the required files.")
+                
+                with open(attributes_path, 'r') as f:
+                    attributes = json.load(f)
+                    for key, value in attributes.items():
+                        setattr(self, key, value)
+                
+                with open(data_path, 'r') as f:
+                    self.data = json.load(f)
+                
+                with open(positive_doc_ids_path, 'r') as f:
+                    self.positive_doc_ids = json.load(f)
+                with open(negative_doc_ids_path, 'r') as f:
+                    self.negative_doc_ids = json.load(f)
+        else:
+            self.retrieval_task = retrieval_task
+            self.task_description = retrieval_task.description
+            self.retrieval_task.load_data(**load_data_kwargs)
 
-        self.proportional_relevance_threshold = proportional_relevance_threshold
-        self.relevance_threshold = relevance_threshold
-        self.negative_sampling = negative_sampling
-        self.synthetic_data = synthetic_data
-        self.data_augmentation_threshold = data_augmentation_threshold
-        
-        if self.relevance_threshold is None:
-            self.set_threshold()
-        
-        self.split = split or self.retrieval_task.description['eval_splits'][-1]
-        query_ids = self.retrieval_task.queries[self.split].keys()
-        self.data = []
-        
-        # if self.negative_sampling or self.synthetic_data:
-        #     if dres is None:
-        #         default_embed = SentenceTransformer("all-MiniLM-L6-v2", device=get_device())
-        #         dres = DenseRetrievalExactSearch(DRESModel(default_embed))
-        #     if not dres.results:
-        #         dres.search(self.retrieval_task.corpus[self.split], self.retrieval_task.queries[self.split], top_k=100, score_function='cos_sim', return_sorted=True)
-        
-        # if self.negative_sampling, use dres.results to augment negative data. if self.synthetic_data then positive data
-        negatives = {}
-        positives = {}
-        for query_id in query_ids:
-            relevant_docs = self.retrieval_task.relevant_docs[self.split][query_id]
-            negatives[query_id] = {doc_id for doc_id, score in relevant_docs.items() if score < self.relevance_threshold}
-            positives[query_id] = {doc_id for doc_id, score in relevant_docs.items() if score >= self.relevance_threshold}
-            if self.negative_sampling and (neg_docs_left := self.data_augmentation_threshold - len(negatives[query_id])) > 0:
-                random_docs = random.sample(list(self.retrieval_task.corpus[self.split].keys()), neg_docs_left)
-                negatives[query_id].update(random_docs)
-                # for doc_id, score in dres.results[query_id].items():
-                #     if score < self.relevance_threshold:
-                #         negatives[query_id].add(doc_id)
-                #     if len(negatives[query_id]) >= self.data_augmentation_threshold:
-                #         break
-            if self.synthetic_data and (pos_docs_left := self.data_augmentation_threshold - len(positives[query_id])) > 0:
-                positives[query_id].update(gen_synthetic_data(self.retrieval_task.queries[self.split][query_id], pos_docs_left))
-            for good_doc_id in positives[query_id]:
-                for bad_doc_id in negatives[query_id]:
-                    self.data.append((query_id, good_doc_id, bad_doc_id))
-        self.negative_doc_ids = negatives
-        self.positive_doc_ids = positives
+            self.proportional_relevance_threshold = proportional_relevance_threshold
+            self.set_threshold()  # run it either way to get min and max relevance scores
+            if relevance_threshold is not None:
+                self.relevance_threshold = relevance_threshold
+            self.negative_sampling = negative_sampling
+            self.synthetic_data = synthetic_data
+            self.data_augmentation_threshold = data_augmentation_threshold
+            
+            if self.relevance_threshold is None:
+                self.set_threshold()
+            
+            self.split = split or self.retrieval_task.description['eval_splits'][-1]
+            query_ids = self.retrieval_task.queries[self.split].keys()
+            self.data = []
+            
+            """ Create a list of triplets for each query. """
+            # if negative_sampling, augment negatives up to data_augmentation_threshold.
+            # if synthetic_data, augment positives up to data_augmentation_threshold.
+            negatives = {}
+            positives = {}
+            for query_id in query_ids:
+                relevant_docs = self.retrieval_task.relevant_docs[self.split][query_id]
+                negatives[query_id] = {doc_id for doc_id, score in relevant_docs.items() if score < self.relevance_threshold}
+                positives[query_id] = {doc_id for doc_id, score in relevant_docs.items() if score >= self.relevance_threshold}
+                if self.negative_sampling and (neg_docs_left := self.data_augmentation_threshold - len(negatives[query_id])) > 0:
+                    random_docs = random.sample(list(self.retrieval_task.corpus[self.split].keys()), neg_docs_left)
+                    negatives[query_id].update(random_docs)
+                if self.synthetic_data and (pos_docs_left := self.data_augmentation_threshold - len(positives[query_id])) > 0:
+                    positives[query_id].update(gen_synthetic_data(self.retrieval_task.queries[self.split][query_id], pos_docs_left))
+                for good_doc_id in positives[query_id]:
+                    for bad_doc_id in negatives[query_id]:
+                        self.data.append((query_id, good_doc_id, bad_doc_id))
+                negatives[query_id] = list(negatives[query_id])
+                positives[query_id] = list(positives[query_id])
+            self.negative_doc_ids = negatives
+            self.positive_doc_ids = positives
 
 
     def set_threshold(self):
@@ -111,3 +137,38 @@ class TripletDataset(Dataset):
             stringify_corpus_item(self.retrieval_task.corpus[self.split][good_doc_id]),
             stringify_corpus_item(self.retrieval_task.corpus[self.split][bad_doc_id])
         )
+
+    def save(self, directory=None):
+        if directory is None:
+            directory = os.path.join(get_proj_dir(), 'datasets', 'cached', self.retrieval_task.description['task'], self.split, 'triplet', datetime.now().strftime('%Y-%m-%d-%H-%M-%S'))
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        
+        # Save attributes
+        attributes = {
+            'proportional_relevance_threshold': self.proportional_relevance_threshold,
+            'relevance_threshold': self.relevance_threshold,
+            'min_relevance_score': self.min_relevance_score,
+            'max_relevance_score': self.max_relevance_score,
+            'negative_sampling': self.negative_sampling,
+            'synthetic_data': self.synthetic_data,
+            'data_augmentation_threshold': self.data_augmentation_threshold,
+            'split': self.split,
+            'task_description': self.task_description
+        }
+        with open(os.path.join(directory, 'attributes.json'), 'w') as f:
+            json.dump(attributes, f, indent=4)
+        
+        # Save dataset
+        with open(os.path.join(directory, 'data.json'), 'w') as f:
+            json.dump(self.data, f, indent=None)
+        
+        # Save positive and negative doc ids
+        with open(os.path.join(directory, 'positive_doc_ids.json'), 'w') as f:
+            json.dump(self.positive_doc_ids, f, indent=None)
+        with open(os.path.join(directory, 'negative_doc_ids.json'), 'w') as f:
+            json.dump(self.negative_doc_ids, f, indent=None)
+    
+    @classmethod
+    def load(cls, directory):
+        return cls(load_dir=directory)

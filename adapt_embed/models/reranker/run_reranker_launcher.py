a4042979d32b8576343cf6c2d24c788e5a0490bb
Suvansh Sanjeev <suvansh.sanjeev@gmail.com>
2024-04-06 12:50:41 -0700

    adding option for only tuning classifier head of reranker, and getting results/plots every n epochs

--- adapt_embed/models/reranker/run_reranker_launcher.py
+++ adapt_embed/models/reranker/run_reranker_launcher.py
@@ -25,6 +25,7 @@ def run_experiment(variant):
     task = variant['task']
     split = variant['split']
     num_epochs = variant['num_epochs']
+    results_every = variant.get('results_every', 0)
     batch_size = variant['batch_size']
     lr = variant['lr']
     results_files = variant.get('results_files', [])
@@ -33,11 +34,17 @@ def run_experiment(variant):
     data_synthetic_gen = variant.get('data_synthetic_gen', False)
     data_synthetic_data_path = variant.get('data_synthetic_data_path', None)
     data_augmentation_threshold = variant.get('data_augmentation_threshold', 10)
+    tune_classifier_only = variant.get('tune_classifier_only', False)
     data_llm = variant.get('data_llm', 'gpt-4-turbo-preview')
 
     model = SentenceTransformer(model_name, device=device)
     reranker_model = Reranker(rerank_model_name).to(device)
-    reranker_baseline_model = Reranker(rerank_model_name).to(device)
+    if tune_classifier_only:
+        # freeze all parameters in the model, then unfreeze just the classification head
+        for param in reranker_model.model.model.parameters():
+            param.requires_grad = False
+        for param in reranker_model.model.model.classifier.parameters():
+            param.requires_grad = True
 
     task_class = MTEB(tasks=[task]).tasks[0]    
     reranker_task = RerankerRetrievalTask.from_task(type(task_class))()
@@ -50,29 +57,39 @@ def run_experiment(variant):
                                                        data_augmentation_threshold=data_augmentation_threshold,
                                                        llm=data_llm))
 
-    reranker_weights_dir = os.path.join(logger.get_snapshot_dir(), 'reranker_model')
-    reranker_model.fit(dataset, num_epochs=num_epochs, lr=lr, batch_size=batch_size, model_save_path=reranker_weights_dir)
-
     def get_results(exp_name, model, task):
         results_dir = os.path.join(logger.get_snapshot_dir(), exp_name)
         os.makedirs(results_dir, exist_ok=True)
         return get_mteb_results(task, os.path.join(results_dir, "results.json"), model=model)
 
-    results_reranked = get_results(exp_name, reranker_model, reranker_task)
-
     external_results = {name: {task: json.load(open(results_file))} for results_file, name in results_files if os.path.exists(results_file)}
-
     baseline_results = get_mteb_results(task, os.path.join(proj_dir, 'results', model_name, f"{task}.json"), model=model)
-    plot_comparison([(baseline_results, "Baseline"),
-                     (results_reranked, "Fine-tuned Reranker"),
-                     *[(results, name) for name, results in external_results.items()]],
-                    exp_name, variant)
+    reranker_weights_dir = os.path.join(logger.get_snapshot_dir(), 'reranker_model')
+    if results_every != 0:
+        total_epochs = 0
+        while (epochs_to_train := min(results_every, num_epochs - total_epochs)) > 0:
+            total_epochs += epochs_to_train
+            reranker_model.fit(dataset, num_epochs=epochs_to_train, lr=lr, batch_size=batch_size, model_save_path=reranker_weights_dir)
+            results_reranked = get_results(f"{exp_name}_{total_epochs}_epochs", reranker_model, reranker_task)
+            plot_comparison([(baseline_results, "Baseline"),
+                             (results_reranked, f"Fine-tuned Reranker ({total_epochs} Epochs)"), 
+                             *[(results, name) for name, results in external_results.items()]],
+                             exp_name=f"{exp_name}_{total_epochs}_epochs",
+                             variant=variant,
+                             save_dir=os.path.join(logger.get_snapshot_dir(), 'imgs', f"{total_epochs}_epochs"))
+    else:
+        reranker_model.fit(dataset, num_epochs=num_epochs, lr=lr, batch_size=batch_size, model_save_path=reranker_weights_dir)
+        results_reranked = get_results(exp_name, reranker_model, reranker_task)
+        plot_comparison([(baseline_results, "Baseline"),
+                        (results_reranked, "Fine-tuned Reranker"),
+                        *[(results, name) for name, results in external_results.items()]],
+                        exp_name, variant)
 
 if __name__ == "__main__":
     """ hyperparameters """
     variants_list = [
         dict(
-            exp_prefix=['reranker'],
+            exp_prefix=['reranker-only-classifier'],
             model_name=["all-MiniLM-L6-v2"],
             rerank_model_name=["distilroberta-base"],
             task=['CQADupstackEnglishRetrieval'],
@@ -83,7 +100,9 @@ if __name__ == "__main__":
             data_negative_sampling=[True, False],
             data_synthetic_gen=[True, False],
             data_augmentation_threshold=[5],
-            data_llm=['claude-3-sonnet-20240229']
+            data_llm=['claude-3-sonnet-20240229'],
+            tune_classifier_only=[True],
+            results_every=[3]
         )
     ]
     results_files = []
